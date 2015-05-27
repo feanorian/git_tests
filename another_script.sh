3c508d4a23d44975e7f337337001e35a549b0c36
Craig L Brooks <Craig.Brooks95@myhunter.cuny.edu>
2015-05-27 10:03:48 -0400

    This is another script version 2.0

--- another_script.sh
+++ another_script.sh
@@ -1,4 +1,20 @@
-#this scripr creates 3 empty files
-for i in {1..3};
-do touch files$i.txt
+#this script makes a new file on the 5th iteration
+#I am adding a new comment lol
+for i in {1..100000};
+do 
+    if [$i == 5] then
+	touch files$i.txt
+    fi
 done
+
+echo $i
+
+#put some contents into a file
+for j in {1..10};
+do 
+    if [$j == 5] then
+	echo "Contents of file $j" > files$j.txt
+    fi
+done
+
+echo $j
