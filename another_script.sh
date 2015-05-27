227409f7c76f0a1114a848781bb9fc82abf42360
Craig L Brooks <Craig.Brooks95@myhunter.cuny.edu>
2015-05-27 10:18:34 -0400

    This is another_script that actually works; put spaces between brackets

--- another_script.sh
+++ another_script.sh
@@ -2,19 +2,20 @@
 #I am adding a new comment lol
 for i in {1..100000};
 do 
-    if [$i == 5] then
+     if [ $i == 5 ]; then
 	touch files$i.txt
-    fi
+     fi
 done
 
 echo $i
 
 #put some contents into a file
+
 for j in {1..10};
 do 
-    if [$j == 5] then
-	echo "Contents of file $j" > files$j.txt
-    fi
+     if [ $j == 5 ]; then
+	 echo "Contents of file $j" > files$j.txt
+     fi
 done
 
 echo $j
